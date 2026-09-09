0287b87b31991cf8838ca6681c2d8143f6daa158
diegosouzasalao-collab <diegosouzasalao@gmail.com>
2026-09-09 20:18:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import requests
 from datetime import datetime, timedelta
 
 # Insira a URL do seu Google Apps Script diretamente aqui ou configure no Render (WEBHOOK_URL)
-URL_WEBHOOK = os.getenv("URL_WEBHOOK = "[https://script.google.com/](https://script.google.com/)...")
-
+URL_WEBHOOK = os.getenv("[https://script.google.com/](https://script.google.com/)...").
 horarios_enviados = set()
 
 def buscar_resultados():
